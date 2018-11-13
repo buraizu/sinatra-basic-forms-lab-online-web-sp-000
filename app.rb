421799b6d338f128489f756e56540ca5cc500149
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-13 03:52:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,12 +10,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/puppy' do
-    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
-    @breed = @puppy.breed
-    @name = @puppy.name
-    @age = @puppy.age
-  end
+  
 
   post '/puppy' do
     @name = params[:name]
